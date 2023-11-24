d9859e448988e907c6e8dae641e243badacdba46
Romina Caubarrere <rominacaubarrere@gmail.com>
2023-11-24 20:14:12 -0300

    FIX: Agrego metodo que borre de utils

--- src/main/java/com/resources/utils/Utils.java
+++ src/main/java/com/resources/utils/Utils.java
@@ -17,7 +17,39 @@ import java.lang.StringBuilder;
 
 // Declaración de la clase Utils
 public class Utils {
+	   // Constante para la ruta base para EJBs
+    private static final String baseRoute = "ejb:/PDTServerSide/";
     
+    // Método genérico para obtener un bean EJB
+    public static <T> T getBean(Class<T> expectedBean) {
+        // Inicializar propiedades para JNDI
+        final Properties env = new Properties();
+        final String sep = "!"; // Separador usado en la ruta JNDI
+        try {
+            // Intentar cargar el archivo de propiedades JNDI
+            try(InputStream jndi = Utils.class.getClassLoader().getResourceAsStream("com/resources/configs/jndi.properties")) {
+                env.load(jndi); // Cargar propiedades desde el archivo
+            }
+            // Crear un contexto inicial con las propiedades JNDI
+            Context ctx = new InitialContext(env);
+            
+            // Construir la ruta JNDI para el bean
+            String route = new StringBuilder(baseRoute)
+                    .append(expectedBean.getSimpleName().replace("Remote", "")) // Añadir el nombre simple del bean esperado
+                    .append(sep) // Añadir el separador
+                    .append(expectedBean.getName()) // Añadir el nombre completo del bean
+                    .toString(); // Convertir a String
+            
+            // Buscar y retornar el bean usando la ruta JNDI
+            return expectedBean.cast(ctx.lookup(route));
+        } catch (IOException e) {
+            e.printStackTrace(); // Imprimir la traza de la excepción IOException
+            return null; // Retornar null en caso de IOException
+        } catch (NamingException e) {
+            e.printStackTrace(); // Imprimir la traza de la excepción NamingException
+            return null; // Retornar null en caso de NamingException
+        }
+    }
     
     public static Usuario checkUser() {
     	
